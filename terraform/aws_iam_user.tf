fa2923e8a952cd01db065231c59a1fc2c17efae2
dehvCurtis <dehvonc@gmail.com>
2021-06-24 15:56:47 -0600

    updated params
    updated params

--- terraform/aws_iam_user.tf
+++ terraform/aws_iam_user.tf
@@ -1,8 +1,8 @@
-resource "krkn_user" "krkn_user" {
-  name = "krkn_user"
-  path = "/krkn_users/"
+resource "abc123_user" "abc123_user" {
+  name = "abc123_user"
+  path = "/abc123_users/"
   policy = aws_iam_policy
   tags = {
-    krkn_user = "krkn_user"
+    krkn_user = "abc123_user"
   }
 }
