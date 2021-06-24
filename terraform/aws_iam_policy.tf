76aa6b598036ba1231ca65eb6315de4ef8b2b64d
dehvCurtis <dehvonc@gmail.com>
2021-06-24 15:39:48 -0600

    created aws_iam_policy.tf
    created aws_iam_policy.tf

---
+++ terraform/aws_iam_policy.tf
@@ -0,0 +1,19 @@
+resource "aws_iam_role" "AssumeIAMRole" {
+  name = "AssumeIAMRole"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "arn:aws:iam::123456789"
+      },
+      "Action": "sts:AssumeRole",
+      "Condition": {}
+    }
+  ]
+}
+EOF
+}
